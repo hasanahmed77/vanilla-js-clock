7b84201a04e000fb3dff20192e2334ce40b837b6
Hasan Ahmed <megatron.kyuubi@gmail.com>
2022-06-23 02:10:44 +0600

    completed the project

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+const secondHand = document.querySelector('[data-second-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const hourHands = document.querySelector('[data-hour-hand]');
+
+const setClock = () => {
+    const currentDate = new Date()
+    const secondRatio = currentDate.getSeconds() / 60
+    const minuteRatio = (secondRatio + currentDate.getMinutes()) / 60
+    const hourRatio = (minuteRatio + currentDate.getHours()) / 12
+
+    setRotation(secondHand, secondRatio);
+    setRotation(minuteHand, minuteRatio);
+    setRotation(hourHands, hourRatio);
+    console.log('animal')
+}
+
+const setRotation = (element, rotationRatio) => {
+    element.style.setProperty('--rotation', rotationRatio * 360)
+} 
+
+setInterval(setClock, 1000)
+setClock()
+
